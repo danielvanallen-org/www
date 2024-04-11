5039fb57cab34a7918dbb0514296a12370991fd9
Daniel Van Allen <danvanallen@gmail.com>
2024-04-11 22:26:45 +0000

    open up ACAO to all origins

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
                 headers: [
                     {
                         key: "Access-Control-Allow-Origin",
-                        value: process.env.NEXT_PUBLIC_APP_URL,
+                        value: "*",
                     },
                 ],
             },
