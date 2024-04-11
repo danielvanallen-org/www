76b7973e6aef52bcbcda4a2f3047164812091a36
Daniel Van Allen <danvanallen@gmail.com>
2024-04-11 21:25:41 +0000

    Try a header change to fix app errors in prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone"
+    output: "standalone",
+    headers: async () => {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: process.env.NEXT_PUBLIC_APP_URL,
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
