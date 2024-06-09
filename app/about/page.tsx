6555e7a761eeafdc79699874602ff73bb9fa707f
Daniel Van Allen <danvanallen@gmail.com>
2024-06-09 23:29:02 +0000

    css: fix scrollbar content jump

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,12 +7,18 @@ export default function About() {
         <br />
         GitHub:&nbsp;
         <a href="https://github.com/dvanallen">github.com/dvanallen</a>
+        <br />
+        This site:&nbsp;
+        <a href="https://github.com/danielvanallen-org/www">github.com/danielvanallen-org/www</a>
       </p>
 
       <h3 className="mt-3">Contact</h3>
       <p>
-        Email:&nbsp;
+        Business:&nbsp;
         <a href="mailto:daniel@danielvanallen.org">daniel@danielvanallen.org</a>
+        <br />
+        Personal:&nbsp;
+        <a href="mailto:danvanallen@gmail.com">danvanallen@gmail.com</a>
         <br />
         PGP:&nbsp;
         <a className="external" href="http://pgp.mit.edu/pks/lookup?search=0x3854878BD47CDADCE1D3D6A63AE5757AC1FD9D12">3854 878B D47C DADC E1D3 D6A6 3AE5 757A C1FD 9D12</a>
