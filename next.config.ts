1b8b5b13a0cd396a1d2be2e4470de40cfd12b98c
Daniel Van Allen <danvanallen@gmail.com>
2025-11-02 05:32:59 +0000

    redirect: unwind nextjs redirect

--- next.config.ts
+++ next.config.ts
@@ -4,23 +4,6 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   output: "standalone",
   reactCompiler: true,
-
-  // Save some money by doing this redirect with NextJS instead of using a load balancer.
-  async redirects() {
-    return [
-      {
-        source: '/:any*',
-        has: [
-          {
-            type: 'host',
-            value: '(www\\.)?cryptcolors\\.com',
-          },
-        ],
-        destination: 'https://www.danielvanallen.org/cryptcolors',
-        permanent: false,
-      },
-    ]
-  },
 }
  
 export default nextConfig
